0a94ca1f2a2461bf78080b096304fc5cc72f3eb9
Nathan Collins <Nathanxcollins@outlook.com>
2023-10-30 07:28:35 -0500

    Add project files.

---
+++ Data/SupabaseService.cs
@@ -0,0 +1,16 @@
+﻿namespace TheSimpleApp.Data
+{
+    // Services/SupabaseService.cs
+    public class SupabaseService
+    {
+        private readonly Supabase.Client _client;
+
+        public SupabaseService()
+        {
+            _client = new Supabase.Client("projectURL", "publicKey");
+        }
+
+        public Supabase.Client Client => _client;
+    }
+
+}
